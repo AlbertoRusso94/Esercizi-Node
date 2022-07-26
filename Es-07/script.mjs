830af9809da71fdbc1452c4a7c63bf1054eb431e
Alberto Russo <alberto9449@gmail.com>
2022-07-26 12:26:33 +0200

    Esercizio

---
+++ Es-07/script.mjs
@@ -0,0 +1,11 @@
+import figlet from "figlet";
+
+console.log(
+  figlet.textSync("Amogus", {
+    font: "Blocks",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  })
+);
